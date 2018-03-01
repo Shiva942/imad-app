eda1835c4be130dd8d4204103ac9e6c9245b7578
Shiva942 <30700067+Shiva942@users.noreply.github.com>
2018-03-01 09:29:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/art1', function(req,res){
-    res.send('Article 1 requested will be served here');
+    res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
 });
 
 app.get('/art2', function(req,res){
