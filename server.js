dc9c92c54a52b0a365a70abfc2f1e3a606c73b7f
Shiva942 <30700067+Shiva942@users.noreply.github.com>
2018-03-19 22:39:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -10,30 +10,28 @@ app.get('/', function (req, res) {
 });
 
 app.get('/ui/art1.html', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'art1.html'));
 });
 
 app.get('/ui/main.js', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
 app.get('/art2', function(req,res){
-    res.send('Article 2 requested will be served here');
+  res.send('Article 2 requested will be served here');
 });
 
 app.get('/art3', function(req,res){
-    res.send('Article 3 requested will be served here');
+  res.send('Article 3 requested will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 80;
 app.listen(port, function () {
